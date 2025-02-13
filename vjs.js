877357469a092d51caf2989559d51b03c7676de7
mohamed <berkaouimed1@gmail.com>
2025-02-13 23:18:14 +0100

    g

--- vjs.js
+++ vjs.js
@@ -6,18 +6,18 @@ const $display1 = document.querySelector(".display1");
 const $display2 = document.querySelector(".display2");
 
 const movingTextPostions = [
-  { top: "10%", left: "20%" },
+  { top: "50%", left: "50%" },
   { top: "60%", left: "30%" },
-  { top: "80%", left: "10%" },
+  { top: "80%", left: "40%" },
   { top: "70%", left: "40%" },
   { top: "30%", left: "30%" },
-  { top: "17%", left: "28%" },
+  { top: "86%", left: "28%" },
   { top: "65%", left: "34%" },
-  { top: "89%", left: "19%" },
+  { top: "89%", left: "39%" },
   { top: "77%", left: "47%" },
-  { top: "36%", left: "36%" },
+  { top: "86%", left: "36%" },
 ];
-const movingTextWords = ["are you sure ?", "think twice","one more","absoulte nej","bisous ?","are you sure ?", "think twice","one more","absoulte nej","bisous ?","are you sure ?", "think twice","one more","absoulte nej","bisous ?"];
+const movingTextWords = ["play the song","are you sure ?", "think twice","one more","absoulte nej","bisous ?","are you sure ?", "Jag älskar dig","one more","Vad heter du?","please ?","are you sure ?", "come on think thrice","Jag saknar dig","I wish i was there","bisous ?","i hope you're enjoying the song"];
 let clickCount = 0;
 
 $btnYes.addEventListener("mouseenter", function () {
