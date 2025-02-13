1d5a90f34c0a41a5c81a11671f1de7a711e18778
mohamed <berkaouimed1@gmail.com>
2025-02-13 15:09:55 +0100

    second

--- vjs.js
+++ vjs.js
@@ -1,48 +1,51 @@
-const [$btnYes,$btnNo]=document.getElementsByClassName('button')
-const $buttonsContainer=document.getElementsByClassName("buttons-container")[0]
-const $movingText=document.getElementById('moving-text')
-const $display1=document.querySelector(".display1")
-const $display2=document.querySelector(".display2")
+const [$btnYes, $btnNo] = document.getElementsByClassName("button");
+const $buttonsContainer =
+  document.getElementsByClassName("buttons-container")[0];
+const $movingText = document.getElementById("moving-text");
+const $display1 = document.querySelector(".display1");
+const $display2 = document.querySelector(".display2");
 
-const movingTextPostions=[{top:"10%",left:'20%'},
-  {top:"60%",left:'30%'},
-  {top:"80%",left:'10%'},
-  {top:"70%",left:'40%'},
-  {top:"30%",left:'30%'},
-  {top:"17%",left:'28%'},
-  {top:"65%",left:'34%'},
-  {top:"89%",left:'19%'},
-  {top:"77%",left:'47%'},
-  {top:"36%",left:'36%'}
-]
-let clickCount=0
+const movingTextPostions = [
+  { top: "10%", left: "20%" },
+  { top: "60%", left: "30%" },
+  { top: "80%", left: "10%" },
+  { top: "70%", left: "40%" },
+  { top: "30%", left: "30%" },
+  { top: "17%", left: "28%" },
+  { top: "65%", left: "34%" },
+  { top: "89%", left: "19%" },
+  { top: "77%", left: "47%" },
+  { top: "36%", left: "36%" },
+];
+const movingTextWords = ["are you sure ?", "think twice","one more","absoulte nej","bisous ?","are you sure ?", "think twice","one more","absoulte nej","bisous ?","are you sure ?", "think twice","one more","absoulte nej","bisous ?"];
+let clickCount = 0;
 
-$btnYes.addEventListener('mouseenter',function(){
-  if(clickCount<5){
-    $btnYes.style.visibility="hidden"
+$btnYes.addEventListener("mouseenter", function () {
+  if (clickCount < 5) {
+    $btnYes.style.visibility = "hidden";
   }
-})
-$buttonsContainer.addEventListener('mouseleave',function(){
-  if(clickCount<5){
-    $btnYes.style.visibility="visible"
+});
+$buttonsContainer.addEventListener("mouseleave", function () {
+  if (clickCount < 5) {
+    $btnYes.style.visibility = "visible";
   }
-})
+});
 
-$btnNo.addEventListener('click',()=>{
-  clickCount++
-  console.log("first")
-  console.log($btnYes.style)
-$btnYes.style.scale=`${(clickCount+11)/10}`
+$btnNo.addEventListener("click", () => {
+  clickCount++;
+  console.log("first");
+  console.log($btnYes.style);
+  $btnYes.style.scale = `${(clickCount + 11) / 10}`;
 
-$btnYes.style.visibility="visible"
-$movingText.style.display="block"
-const randomPos=Math.floor(Math.random()*(movingTextPostions.length+1))
-$movingText.style.top=movingTextPostions[randomPos].top
-$movingText.style.left=movingTextPostions[randomPos].left
-})
+  $btnYes.style.visibility = "visible";
+  $movingText.style.display = "block";
+$movingText.innerHTML=movingTextWords[clickCount-1]
+  const randomPos = Math.floor(Math.random() * (movingTextPostions.length + 1));
+  $movingText.style.top = movingTextPostions[randomPos].top;
+  $movingText.style.left = movingTextPostions[randomPos].left;
+});
 
-$btnYes.addEventListener('click',function(){
-  $display1.style.display="none"
-  $display2.style.display="flex"
-
-})
+$btnYes.addEventListener("click", function () {
+  $display1.style.display = "none";
+  $display2.style.display = "flex";
+});
